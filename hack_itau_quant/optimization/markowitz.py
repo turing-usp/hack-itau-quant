Fix magic number calculation and test ef plot (solver, py opt and hyperbola)

hack_itau_quant/optimization/markowitz.py
@@ -49,26 +49,25 @@ class Markowitz:
         W = np.linalg.inv(self._cov_matrix)
         e = np.ones((self._n_assets, 1))
 
-        We = W @ e
-        Wm = W @ self._expected_returns
+        We = np.dot(W, e)
+        Wm = np.dot(W, self._expected_returns)
 
         return e, We, Wm
 
     def _build_magic_numbers(self):
         e, We, Wm = self._build_useful_matrices()
 
-        A = e.T @ Wm
-        B = self._expected_returns.T @ We
-        C = e.T @ We
+        A = np.dot(e.T, Wm)
+        B = np.dot(self._expected_returns.T, Wm)
+        C = np.dot(e.T, We)
         D = B * C - A**2
 
         return A, B, C, D, We, Wm
 
     def _solve(self):
-        A, B, C, D, We, Wm = self._build_magic_numbers()
 
         x_min = self._We / self._C
-        z = Wm - self._A/self._C * self._We
+        z = self._Wm - (self._A/self._C) * self._We
 
         return x_min, z
 

test.py
@@ -18,9 +18,11 @@ cov_matrix = returns.cov()
 
 ef = EfficientFrontier(expected_returns, cov_matrix)
 
-w1 = ef.efficient_return(0.1)
+target_return = 0.0014
+target_risk = 0.024
 
-w2 = ef.efficient_risk(0.1)
+w1 = ef.efficient_return(target_return)
+w2 = ef.efficient_risk(target_risk)
 
 print("EFFICIENT RETURN: ", w1)
 
@@ -29,31 +31,42 @@ print("EFFICIENT RISK: ", w2)
 
 
 e = ef_opt(expected_returns, cov_matrix)
-# e.min_volatility()
 
-fig, ax = plt.subplots()
-plotting.plot_efficient_frontier(e, ax=ax, show_assets=False, ef_param ='risk')
-plt.savefig('pyopt.png')
+try:
+    print("EFFICIENT RETURN: ", e.efficient_return(target_return))  
+except:
+    pass
 
-returns, risks = ef.plot_efficient_frontier(0.0005, 70)
-plt.plot(risks, returns, label='Hack')
+try:
+    print("EFFICIENT RISK: ", e.efficient_risk(target_risk**2))
+except:
+    pass
+
+# fig, ax = plt.subplots()
+# plotting.plot_efficient_frontier(e, ax=ax, show_assets=False)
+# plt.savefig('pyopt.png')
+
+returns, risks = ef.plot_efficient_frontier(0.00005, 30)
+plt.plot(risks, returns, label='Otimizador Hack - Hyperbola')
+plt.legend()
 plt.savefig('test_hyperbola.png')
 
 
 
-# returns, risks = [], []
+returns, risks = [], []
 
-# for r in tqdm(np.arange(0.02, 0.09, 0.0005)):
+for r in tqdm(np.arange(0.0009, 0.0022, 0.00005)):
 
-#     w = ef.efficient_return(r).reshape(4, 1)
+    w = ef.efficient_return(r).reshape(4, 1)
 
-#     rs = np.dot(w.T, expected_returns * 252)
+    rs = np.dot(w.T, expected_returns)
 
-#     sigma = np.dot(w.T, np.dot(cov_matrix * np.sqrt(252), w))
+    sigma = np.sqrt(np.dot(w.T, np.dot(cov_matrix, w)))
 
-#     returns.append(float(rs))
-#     risks.append(float(sigma))
+    returns.append(float(rs))
+    risks.append(float(sigma))
 
 
-# plt.plot(np.array(risks) * np.sqrt(252), np.array(returns) * 1)
-# plt.savefig('test_solver.png')
+plt.plot(np.array(risks), np.array(returns), label='Otimizador Hack - Solver')
+plt.legend()
+plt.savefig('test_solver.png')