2a449b1b5dcb18bcecf3e3a5c8a1ebee24e9bcde
Guilherme Fernandes <aateg@live.com>
2021-05-08 13:57:10 -0300

    add andre code

--- hack_itau_quant/efficient_frontier.py
+++ hack_itau_quant/efficient_frontier.py
@@ -2,15 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from .optimization import Markowitz
 
+import plotly.offline as py
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+
 
 class EfficientFrontier:
 
     def __init__(self, expected_returns: np.array, cov_matrix: np.array):
-        self._expected_returns = expected_returns.to_numpy()
-        self._cov_matrix = cov_matrix.to_numpy()
+        self._expected_returns = expected_returns
+        self._cov_matrix = cov_matrix
         self._solver = Markowitz(
-            expected_returns=self._expected_returns,
-            cov_matrix=self._cov_matrix)
+            expected_returns=self._expected_returns.to_numpy(),
+            cov_matrix=self._cov_matrix.to_numpy())
 
     def efficient_risk(self, target_risk):
         return self._solver.optimal_risk(target_risk)
@@ -22,15 +26,7 @@ class EfficientFrontier:
 
         returns, risks, weigths = self._solver.get_efficient_curve(n_points)
         
-        plt.plot(risks, returns)
-
-        plt.xlabel('Volatilities')
-        plt.ylabel('Returns')
-        plt.title('Efficient Frontier')
-
-        plt.show()
-
-        return (returns, risks, weigths)
+        self._plot(returns, risks, weigths)
 
     def max_loss(self, loss, period, z_alpha=-1.645):
 
@@ -39,9 +35,9 @@ class EfficientFrontier:
         for r in np.arange(start_return, 1, 0.00005):
 
             w = self.efficient_return(r).reshape(-1)
-            rp = np.dot(w.T, self._expected_returns) * period
+            rp = np.dot(w.T, self._expected_returns.to_numpy()) * period
             sigma_p = np.sqrt(
-                np.dot(w.T, np.dot(self._cov_matrix * np.sqrt(period), w)))
+                np.dot(w.T, np.dot(self._cov_matrix.to_numpy() * np.sqrt(period), w)))
 
             z = (loss - rp) / (sigma_p / np.sqrt(period))
 
@@ -50,3 +46,82 @@ class EfficientFrontier:
                 return w
 
         return None
+
+    def plot(self, returns, risks, weigths):
+
+        df = pd.DataFrame(weights)
+        df = (df * 100).round(2).astype(float)
+
+        df['Returns'] = returns
+        df['Volatilies'] = risks
+        df['Sharpe'] = (df['Returns']) / (df['Volatilies'])
+
+        max_sharpe = df[df.Sharpe == df.Sharpe.max()]
+        min_vol = df[df.Volatilies == df.Volatilies.min()]
+
+        Efficient_Frontier = go.Scatter(x = df['Volatilies'],
+                            y = df['Returns'],
+                            text= df['Sharpe'],
+                            customdata = np.stack((df['BPAC11.SA'], df['ITUB4.SA'], df['OIBR3.SA'], df['PETR4.SA']), axis = -1),
+                            hovertemplate=
+                            "Volatilities: %{x:,.2f}<br>"+
+                            "Returns: %{y:,.2f}<br>"+
+                            "Sharpe: %{text:.5f}<br>"+
+                            "BPAC11.SA: %{customdata[0]}%<br>"+
+                            "ITUB4.SA: %{customdata[1]}%<br>"+
+                            "OIBR3.SA: %{customdata[2]}%<br>"+
+                            "PETR4.SA: %{customdata[3]}%<br>",
+                            mode = ' markers + lines',
+                            showlegend = False)
+
+        Max_Sharpe = go.Scatter(x = max_sharpe.Volatilies,
+                            y = max_sharpe.Returns,
+                            text=df[df['Volatilies']  == float(max_sharpe.Volatilies)]['Sharpe'],
+                            customdata = np.stack((df[df['Volatilies']  == float(max_sharpe.Volatilies)]['BPAC11.SA'], 
+                                                df[df['Volatilies']  == float(max_sharpe.Volatilies)]['ITUB4.SA'], 
+                                                df[df['Volatilies']  == float(max_sharpe.Volatilies)]['OIBR3.SA'], 
+                                                df[df['Volatilies']  == float(max_sharpe.Volatilies)]['PETR4.SA']), 
+                                                axis = -1),
+                            hovertemplate=
+                            "Volatilities: %{x:,.2f}<br>"+
+                            "Returns: %{y:,.5f}<br>"+
+                            "Sharpe: %{text:.4f}<br>"+
+                            "BPAC11.SA: %{customdata[0]}%<br>"+
+                            "ITUB4.SA: %{customdata[1]}%<br>"+
+                            "OIBR3.SA: %{customdata[2]}%<br>"+
+                            "PETR4.SA: %{customdata[3]}%<br>",
+                            mode = ' markers + lines',
+                            name = 'Maximum Sharpe')
+
+        Min_Vol = go.Scatter(x = min_vol.Volatilies,
+                            y = min_vol.Returns,
+                            text=df[df['Volatilies']  == float(min_vol.Volatilies)]['Sharpe'],
+                            customdata = np.stack((df[df['Volatilies']  == float(min_vol.Volatilies)]['BPAC11.SA'], 
+                                                df[df['Volatilies']  == float(min_vol.Volatilies)]['ITUB4.SA'], 
+                                                df[df['Volatilies']  == float(min_vol.Volatilies)]['OIBR3.SA'], 
+                                                df[df['Volatilies']  == float(min_vol.Volatilies)]['PETR4.SA']), 
+                                                axis = -1),
+                            hovertemplate=
+                            "Volatilities: %{x:,.2f}<br>"+
+                            "Returns: %{y:,.5f}<br>"+
+                            "Sharpe: %{text:.4f}<br>"+
+                            "BPAC11.SA: %{customdata[0]}%<br>"+
+                            "ITUB4.SA: %{customdata[1]}%<br>"+
+                            "OIBR3.SA: %{customdata[2]}%<br>"+
+                            "PETR4.SA: %{customdata[3]}%<br>",
+                            mode = ' markers + lines',
+                            name = 'Minimal Volatility')
+        
+
+        layout = go.Layout(title='Efficient Frontier',
+                        title_x=0.5,
+                        title_y=0.85,
+                        yaxis={'title':'Returns',
+                                'tickformat':'.2f'},
+                        xaxis={'title':'Volatities',
+                                'tickformat':'.2f'},)
+
+        fig = go.Figure(data = [Efficient_Frontier, Max_Sharpe, Min_Vol], layout = layout)
+
+
+        fig.show()
